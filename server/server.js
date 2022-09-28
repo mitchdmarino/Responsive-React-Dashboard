b5036710941d60a8e2605282c8e45b8cb427684a
mitchdmarino <mitchdmarino@gmail.com>
2022-09-28 12:23:16 -0700

    setup backend server.js file

--- server/server.js
+++ server/server.js
@@ -1,1 +1,22 @@
+// required packages
+require("./models");
+const express = require("express");
+const cors = require("cors");
 
+// App configuration and MiddleWares
+const app = express();
+const PORT = process.env.PORT || 8000;
+app.use(cors());
+app.use(express.json()); // for json req.bodies
+
+// Routes and Controllers
+
+app.get("/", (req, res) => {
+  res.json({ msg: "Backend Home" });
+});
+
+// Listening on port
+
+app.listen(PORT, () => {
+  console.log(`Server successfully listening on Port ${PORT}`);
+});
