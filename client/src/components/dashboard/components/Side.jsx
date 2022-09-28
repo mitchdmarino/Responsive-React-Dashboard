2502a97dce014a8ed26ec61b2f920bbc9ddc7e8c
mitchdmarino <mitchdmarino@gmail.com>
2022-09-28 15:47:24 -0700

    styling for side-dash

--- client/src/components/dashboard/components/Side.jsx
+++ client/src/components/dashboard/components/Side.jsx
@@ -1,5 +1,12 @@
 export default function Side () {
     return (
-        <div className="dash-side">Side</div>
+        <div className="dash-side">
+            <ul>
+                <li>Balance</li>
+                <li>Deposit Funds</li>
+                <li>Withdraw Funds</li>
+                <li>History</li>
+            </ul>
+        </div>
     )
 }
