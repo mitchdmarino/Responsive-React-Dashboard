b8bed552b50028062f276f33eb048ec89efce456
mitchdmarino <mitchdmarino@gmail.com>
2022-09-28 14:33:51 -0700

    login/logout functions

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { useState, useEffect } from "react";
+import jwt_decode from "jwt-decode";
+import axios from "axios";
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(null);
+  // useEffect -- If the User navigates away from the page, we will log them back in.
+  useEffect(() => {
+    // The token is checked to see if it is currently in storage.
+    const token = localStorage.getItem("jwt");
+    if (token) {
+      // If it is, we will decode it and set the Account in app state.
+      const loggedInUser = jwt_decode(token);
+      setCurrentUser(loggedInUser);
+      //  The authorization headers are created.
+      const options = {
+        headers: {
+          Authorization: token,
+        },
+      };
+      // The app is updated with the account's latest information.
+      axios
+        .get(
+          `${process.env.REACT_APP_SERVER_URL}/user/${loggedInUser.id}`,
+          options
+        )
+        .then((response) => {})
+        .catch((err) => {
+          console.log(err);
+        });
+    } else {
+      setCurrentUser(null);
+    }
+  }, []);
+
+  // This event handler is used to log out the Account when the user has completed their session.
+  const handleLogout = () => {
+    // Local storage is checked for a token.
+    if (localStorage.getItem("jwt")) {
+      // If it exists, delete it.
+      localStorage.removeItem("jwt");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <header></header>
+      <main>
+        <Routes>{/* <Route path="/" element={} */}</Routes>
+      </main>
+    </Router>
   );
 }
 
