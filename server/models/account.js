33f4d1f40c670a2e0b1c5641af4884583332cdc7
mitchdmarino <mitchdmarino@gmail.com>
2022-09-28 12:42:55 -0700

    Makes account schema to handle monetary balance, relationship to User

---
+++ server/models/account.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+// Schema for Monetary Account balance.
+const AccountSchema = new mongoose.Schema(
+  {
+    balance: {
+      type: Number,
+      default: 0,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Account", UserSchema);
--- server/models/user.js
+++ server/models/user.js
@@ -14,6 +14,10 @@ const UserSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    account: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Account",
+    },
   },
   {
     timestamps: true,
