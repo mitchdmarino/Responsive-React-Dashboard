1d4ec97b859d82f7c4ccd5a2c62771865221dafb
mitchdmarino <mitchdmarino@gmail.com>
2022-09-28 19:40:05 -0700

    adds edit profile section

--- client/src/components/user/pages/Profile.jsx
+++ client/src/components/user/pages/Profile.jsx
@@ -1,14 +1,55 @@
+import React from "react"
+import { useState } from "react"
+import axios from "axios"
 
-export default function Profile({currentUser}) {
-    // state for the secret message (aka user privileged data )
+export default function Profile({currentUser, setCurrentUser}) {
+    const [showEdit, setShowEdit] = useState(false)
+    const [name, setName] = useState(currentUser.name)
+    const [email, setEmail] = useState(currentUser.email)
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        try {
+            // authorization header
+            const token = localStorage.getItem("jwt")
+            const options = {
+                headers: {
+                    Authorization: token,
+                },
+            }
+            // Form data to post to the backend
+            const reqBody = {
+                name: name,
+                email: email
+            }
+            const response = await axios.put(
+                `${process.env.REACT_APP_SERVER_URL}/user`, reqBody, options
+            )
+            setCurrentUser(response.data)
+            setShowEdit(false)
+        } catch (error) {
+            console.warn(error)
+        }
+    }
 
     return (
 
-        <div>
-            <h1>Hello {currentUser.name}</h1>
-            
-            <p>Email: {currentUser.email}</p>
-        
+        <div className="profile-page">
+           {!showEdit?  (<div className="profile-info">
+                <h1>Name: {currentUser.name}</h1>
+                <h2>Email: {currentUser.email}</h2>
+                <button onClick={() => setShowEdit(true)}>Edit</button>
+
+            </div>): (
+                <div className="profile-edit">
+                    <label htmlFor="name">Name</label>
+                    <input type="text" value={name} name="name" id="name" onChange={(e) => setName(e.target.value)}/>
+                    <label htmlFor="email">Email</label>
+                    <input type="text" value={email} name="email" id="email" onChange={(e) => setEmail(e.target.value)}/>
+                    <button onClick={() => setShowEdit(false)}>Cancel</button>
+                    <button onClick={(e) => handleSubmit(e)}>Confirm Changes</button>
+                </div>
+            )}
         </div>
     )
 }
