b308b30b40376ada3b294a4dec3e4a3227ff32b6
mitchdmarino <mitchdmarino@gmail.com>
2022-09-29 13:50:49 -0700

    styling of profile page

--- client/src/components/user/pages/Profile.jsx
+++ client/src/components/user/pages/Profile.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { useState } from "react"
 import axios from "axios"
+import { centsToDollar } from "../../utils/centsToDollar"
 
 export default function Profile({currentUser, setCurrentUser}) {
     const [showEdit, setShowEdit] = useState(false)
@@ -35,19 +36,26 @@ export default function Profile({currentUser, setCurrentUser}) {
     return (
 
         <div className="profile-page">
+            <h1>Profile Information</h1>
            {!showEdit?  (<div className="profile-info">
-                <h1>Name: {currentUser.name}</h1>
-                <h2>Email: {currentUser.email}</h2>
-                <button onClick={() => setShowEdit(true)}>Edit Info</button>
+                <p><strong>Name:</strong> {currentUser.name}</p><hr></hr>
+                <p><strong>Email:</strong> {currentUser.email}</p><hr></hr>
+                <button className="button-edit" onClick={() => setShowEdit(true)}>Edit Info</button>
+                <p><strong>Current Balance:</strong> {centsToDollar(currentUser.balance)}</p>
+                
 
             </div>): (
                 <div className="profile-edit">
-                    <label htmlFor="name">Name</label>
-                    <input type="text" value={name} name="name" id="name" onChange={(e) => setName(e.target.value)}/>
-                    <label htmlFor="email">Email</label>
-                    <input type="text" value={email} name="email" id="email" onChange={(e) => setEmail(e.target.value)}/>
-                    <button onClick={() => setShowEdit(false)}>Cancel</button>
-                    <button onClick={(e) => handleSubmit(e)}>Confirm Changes</button>
+                    <form className="user-form" onSubmit={(e) => handleSubmit(e)}>
+                        <label htmlFor="name">Name</label>
+                        <input type="text" value={name} name="name" id="name" onChange={(e) => setName(e.target.value)}/>
+                        <label htmlFor="email">Email</label>
+                        <input type="text" value={email} name="email" id="email" onChange={(e) => setEmail(e.target.value)}/>
+                        <div className="button-group">
+                            <button id="cancel-button" onClick={() => setShowEdit(false)}>Cancel</button>
+                            <button type="submit">Confirm Changes</button>\
+                        </div>
+                    </form>
                 </div>
             )}
         </div>
